94507e6f3f5367993ff4b33434b96b5539557095
Leop0ld <l3opold7@gmail.com>
2017-01-04 23:25:30 +0900

    Add create function simply

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import update from 'react-addons-update';
 import ContactInfo from './ContactInfo';
 import ContactDetails from './ContactDetails';
+import ContactCreate from './ContactCreate';
 
 export default class Contact extends React.Component {
     
@@ -106,6 +107,9 @@ export default class Contact extends React.Component {
                 <ContactDetails
                     isSeleted={this.state.selectedKey != -1}
                     contact={this.state.contactData[this.state.selectedKey]}
+                />
+                <ContactCreate
+                    onCreate={this.handleCreate}
                 />
             </div>
         );
