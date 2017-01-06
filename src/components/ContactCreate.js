e89562293111be099abe831324e16281e2d0be26
Leop0ld <l3opold7@gmail.com>
2017-01-06 23:07:38 +0900

    Add function that press ENTER for Adding

--- src/components/ContactCreate.js
+++ src/components/ContactCreate.js
@@ -11,6 +11,7 @@ export default class ContactCreate extends React.Component {
 
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     }
 
     handleChange(e) {
@@ -30,7 +31,13 @@ export default class ContactCreate extends React.Component {
         this.setState({
             name: '',
             phone: ''
-        })
+        });
+    }
+
+    handleKeyPress(e) {
+        if(e.charCode === 13) {
+            this.handleClick();
+        }
     }
 
     render() {
@@ -51,6 +58,7 @@ export default class ContactCreate extends React.Component {
                         placeholder="phone"
                         value={this.state.phone}
                         onChange={this.handleChange}
+                        onKeyPress={this.handleKeyPress}
                     />
                 </p>
                 <button onClick={this.handleClick}>Create</button>
